a795eaa1a1635c4f824f27c331b5a3bc77ce5194
dima333750 <10292009+dima333750@users.noreply.github.com>
2019-01-11 05:53:33 +0300

    Update updateCloudFlare.sh

--- updateCloudFlare.sh
+++ updateCloudFlare.sh
@@ -5,13 +5,13 @@ IPv4=true
 IPv6=false
 Logfile="/var/log/ddns.log"
 URL="https://www.example.com/updateCloudFlare.php"
-Token="***Insert Your Key Here***"
+
 ##############################################
 
 date >> $Logfile
 if $IPv4; then
-  wget -4qO- --post-data="auth=$Token" $URL >> $Logfile
+  wget -4qO- $URL >> $Logfile
 fi
 if $IPv6; then
-  wget -6qO- --post-data="auth=$Token" $URL >> $Logfile
+  wget -6qO- $URL >> $Logfile
 fi
